597506e2494d162d874d9a2c254a813e71390e09
fendor <fendor@users.noreply.github.com>
2024-10-22 10:31:37 +0200

    Fix windows CI (#1149)
    * Experiment with vscode/test-cli package

---
+++ .vscode-test.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('@vscode/test-cli');
+
+module.exports = defineConfig([
+  {
+    label: 'integration-tests',
+    files: 'out/test/**/*.test.js',
+    version: 'stable',
+    workspaceFolder: './test-workspace',
+    installExtensions: ['justusadam.language-haskell'],
+    mocha: {
+      timeout: 120 * 1000 // 2 minute timeout
+    }
+  }
+  // you can specify additional test configurations, too
+]);
