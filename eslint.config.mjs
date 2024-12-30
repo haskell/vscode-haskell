de6b6b5cd254684b67cd0158059e3fe8ee131032
Fendor <fendor@posteo.de>
2024-12-30 13:42:37 +0100

    More typechecking and fix the type errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,31 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
   { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.node } },
   {
-    ...pluginJs.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname },
+    },
+  },
+  {
+    // disables type checking for this file only
+    files: ['eslint.config.mjs'],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
     rules: {
+      // turn off these lints as we access workspaceConfiguration fields.
+      // So far, there was no bug found with these unsafe accesses.
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      // Sometimes, the 'any' just saves too much time.
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'error',
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
@@ -24,5 +40,4 @@ export default [
       ],
     },
   },
-  ...tseslint.configs.recommended,
-];
+);
