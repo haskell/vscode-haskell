3d9e3341247d69aa570868b36c400f87765213dd
fendor <fendor@posteo.de>
2026-06-12 13:58:41 +0200

    Remove language-haskell extension dependency

--- .vscode-test.js
+++ .vscode-test.js
@@ -6,7 +6,6 @@ module.exports = defineConfig([
     files: 'out/test/**/*.test.js',
     version: 'stable',
     workspaceFolder: './test-workspace',
-    installExtensions: ['justusadam.language-haskell'],
     mocha: {
       timeout: 120 * 1000, // 2 minute timeout
     },
